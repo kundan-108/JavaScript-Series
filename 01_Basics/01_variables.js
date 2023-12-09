80354ae958c865b16d38f8da6c1d996b9bec461a
kundan-108 <114014727+kundan-108@users.noreply.github.com>
2023-12-09 18:22:03 +0000

    variable, data type and coversion ops done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 12345
+let accountEmail = "kundan@github.com"
+let accountPassword = "kundan@123"
+accountCity = "Bengaluru"
+let accountState;
+
+console.log(accountEmail)
+console.log(accountCity)
+//single comment
+/*
+multiple line comment
+short cut for comment - 
+CTR+/
+*/
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+console.log("kundan")
+
+
+let name = "kundan"
+let age = 25
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => big integer
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined type is undefined
+console.log(typeof null); // null is an object
